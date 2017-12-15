09f7f3b6bc4fdd43c4ffa501900616b227d46063
sontrinh <30944570+sontrinh@users.noreply.github.com>
2017-12-15 15:28:08 +0700

    Update find sub-reddit

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -1,4 +1,4 @@
-const subredditRegex = /[r]\/([a-z0-9\-_]+)/;
+const subredditRegex = /[r]\/([a-z0-9\-_]+)/i;
 
 regexpEscape = (s) => {
   return String(s).replace(/[\\^$*+?.()|[\]{}]/g, '\\$&');
@@ -34,6 +34,7 @@ makeSearchQuery = (subreddit, querystring) => {
 
 findSubreddit = (s) => {
   if (!s) return null;
+
   result = s.match(subredditRegex);
   if (result && result.length >= 2) return result[1];
   return null;
