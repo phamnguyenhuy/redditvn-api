5034ae899424d56050b3a6ee89e20441483f4580
sontrinh <30944570+sontrinh@users.noreply.github.com>
2017-12-18 13:36:20 +0700

    Update /users

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,19 +1,25 @@
 const { ServerError } = require('../helpers/server');
 const { user } = require('../services');
 const { findUserById, findUsersCount, findUsersList, findUsersTop } = user;
+const moment = require('moment');
 
 module.exports.getUserById = (user_id) => {
   return findUserById(user_id);
 }
 
-module.exports.getUsersCount = () => {
-  return findUsersCount();
+module.exports.getUsersCount = async () => {
+  return { count: await findUsersCount() };
 }
 
 module.exports.getUsersList = (q, page, limit) => {
   return findUsersList(q, page, limit);
 }
 
-module.exports.getUsersTop = (since, until, limit) => {
-  return findUsersTop(since, until, limit);
+module.exports.getUsersTop = async (since, until, limit) => {
+  var users = findUsersTop(since, until, limit);
+  return {
+    since,
+    until,
+    docs: users
+  };
 }
