5074f38f35a49d86358ce1699638e765a7b74e7b
sontrinh <30944570+sontrinh@users.noreply.github.com>
2017-12-18 13:23:13 +0700

    Change route position

--- src/routes/index.js
+++ src/routes/index.js
@@ -31,16 +31,16 @@ router.use(checkDbConnection);
 router.use(checkPageLimit);
 router.use(checkSinceUntil);
 
-router.get('/posts/:post_id', c(post.getPostById, (req, res, next) => [req.params.post_id]));
-router.get('/posts/:post_id/attachments', c(facebook.getAttachmentsByPostId, (req, res, next) => [req.params.post_id]));
-router.get('/posts/:post_id/comments', c(comment.getCommentsByPostId, (req, res, next) => [req.params.post_id, req.query.page, req.query.limit]));
-router.get('/posts/:post_id/comments-merge', c(comment.getCommentsByPostIdOld, (req, res, next) => [req.params.post_id]));
-
 router.get('/posts/count', c(post.getPostsCount, (req, res, next) => [req.query.since, req.query.until]));
 router.get('/posts/count/comments', c(comment.getCommentsCount, (req, res, next) => [req.query.since, req.query.until]));
 
 router.get('/posts/top/likes', c(post.getPostsOrderByLikes, (req, res, next) => [req.query.since, req.query.until, req.query.limit]));
 router.get('/posts/top/comments', c(post.getPostsOrderByComments, (req, res, next) => [req.query.since, req.query.until, req.query.limit]));
+
+router.get('/posts/:post_id/attachments', c(facebook.getAttachmentsByPostId, (req, res, next) => [req.params.post_id]));
+router.get('/posts/:post_id/comments', c(comment.getCommentsByPostId, (req, res, next) => [req.params.post_id, req.query.page, req.query.limit]));
+router.get('/posts/:post_id/comments-merge', c(comment.getCommentsByPostIdOld, (req, res, next) => [req.params.post_id]));
+router.get('/posts/:post_id', c(post.getPostById, (req, res, next) => [req.params.post_id]));
 
 router.get('/random', c(post.getPostByRandom, (req, res, next) => [req.query.r, req.query.q]));
 router.get('/search', c(post.getPostsBySearch, (req, res, next) => [req.query.r, req.query.q, req.query.since, req.query.until, req.query.page, req.query.limit]));
@@ -49,20 +49,17 @@ router.get('/r/top', c(subreddit.getSubredditTop, (req, res, next) => [req.query
 router.get('/r/:subreddit', c(post.getPostsBySubreddit, (req, res, next) => [req.params.subreddit, req.query.page, req.query.limit]));
 router.get('/r', c(subreddit.getSubreddits, (req, res, next) => [req.query.limit]));
 
-router.get('/users/count', c(user.getUsersCount, (req, res, next) => []));
 router.get('/users/top', c(user.getUsersTop, (req, res, next) => [req.query.since, req.query.until, req.query.limit]));
-router.get('/users/:user_id', c(user.getUserById, (req, res, next) => [req.params.user_id]));
+router.get('/users/count', c(user.getUsersCount, (req, res, next) => []));
 router.get('/users/:user_id/posts', c(post.getPostsByUserId, (req, res, next) => [req.params.user_id, req.query.page, req.query.limit]));
+router.get('/users/:user_id', c(user.getUserById, (req, res, next) => [req.params.user_id]));
 router.get('/users', c(user.getUsersList, (req, res, next) => [req.query.q, req.query.page, req.query.limit]));
 
 router.get('/stats/chart', c(stats.getStatsChart, (req, res, next) => [req.query.type, req.query.group]));
 
 // Catch 404 and forward to error handler
 router.use((req, res, next) => {
-  const error = new Error();
-  error.code = 404;
-  error.message = 'the page you requested does not exist';
-  next(error);
+  next(new ServerError('the page you requested does not exist', 404));
 });
 
 /**
